8a740ab89d8ede0ed6ba040b57102c65d1ea75c2
Kirsten Loechl <kirsten.loechl7@gmail.com>
2018-05-23 12:43:38 -0700

    added files2

--- Lab7Firebase/Lab7Firebase/School.swift
+++ Lab7Firebase/Lab7Firebase/School.swift
@@ -2,24 +2,8 @@
 //  School.swift
 //  Lab7Firebase
 //
-//  Created by Lucas Robertson on 5/20/18.
+//  Created by Kirsten M Loechl on 5/23/18.
 //  Copyright © 2018 lrober09. All rights reserved.
 //
 
 import Foundation
-import Firebase
-import MapKit
-
-struct School : Codable {
-    
-    var name : String
-    var city : String
-    var state : String
-    var zip : String
-    var contact_email : String
-    var latitude : Double
-    var longitude : Double
-    
-    
-}
-
